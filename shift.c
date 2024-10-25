f8fbc7d9fbded772707758c3b45220ab008a863c
carlyle-felix <carlyle.felix@gmail.com>
2024-10-25 23:31:06 +0200

    add view month function
    print monthly calendar populated with shift pattern

--- shift.c
+++ shift.c
@@ -5,32 +5,72 @@
 // function prototypes
 int get_day_of_year(int day, int month, int year, int calendar[], int *year_total);
 void check_date(int ref_year, int ref_day_of_year, int ref_year_total, int calendar[]);
+void view_month(int ref_year, int ref_day_of_year, int ref_year_total, int calendar[], int ref_day_name);
 
 int main(void) {
 
     int calendar[12] = {31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
-    int day, month, year, year_total, day_of_year;
+    int day, month, year, year_total, day_of_year, day_of_week;
     char c, choice;
+
     printf("Enter first day shift of current cycle (dd/mm/yy): ");
     scanf("%d/%d/%d", &day, &month, &year);
+    printf("Enter day of week for first day shift (Sun: 1 — Sat: 7): ");
+    scanf("%d", &day_of_week);
+
     day_of_year = get_day_of_year(day, month, year, calendar, &year_total);
     
     for (;;) {
-        check_date(year, day_of_year, year_total, calendar);
-        
-        printf("\nCheck another date or Quit?\n(C/Q): ");
-        while ((c = toupper(getchar())) != 'C' && c != 'Q');
-        if (c == 'C')   continue;
+
+        printf("\nCheck date, View month or Quit?\n(C/V/Q): ");
+        while ((c = toupper(getchar())) != 'C' && c != 'Q' && c != 'V');
+        if (c == 'C')   check_date(year, day_of_year, year_total, calendar);
+        if (c == 'V')   view_month(year, day_of_year, year_total, calendar, day_of_week - 1);
         if (c == 'Q')   break;
     }
     return 0;
+}
+
+void view_month(int ref_year, int ref_day_of_year, int ref_year_total, int calendar[], int ref_day_of_week) {
+
+    int i, day = 1, month, year, year_total, days_since_ref, sched, day_of_week;
+    char shift_pattern[8] = {'D', 'D', 'N', 'N', 'O', 'O', 'O', 'O'};
+    
+    printf("Enter future month(mm/yy): ");
+    scanf("%d/%d", &month, &year);
+
+    if (ref_year == year) {
+        days_since_ref = get_day_of_year(day, month, year, calendar, &year_total) - ref_day_of_year;
+    } else {
+        days_since_ref = (ref_year_total - ref_day_of_year) + get_day_of_year(day, month, year, calendar, &year_total);
+    }
+    int calendar_month[calendar[month - 1]];
+    
+    day_of_week = (days_since_ref - ref_day_of_week) % 7;
+
+    printf("\n Sun   Mon   Tue   Wed   Thu   Fri   Sat\n");
+    for (i = 0; i < calendar[month - 1] + day_of_week; i++) {
+        if (i < day_of_week) {
+            printf("      ");
+        } else {
+            calendar_month[i - day_of_week] = i + 1 - day_of_week;
+            sched = days_since_ref++ % 8;
+            printf("%2d.%-3c", calendar_month[i - day_of_week], shift_pattern[sched]);
+        }
+        if (i == 6) {
+            printf("\n");
+        } else if (i > 7 && (i + 1) % 7 == 0) {
+            printf("\n");
+        }
+    }
+
 }
 
 void check_date(int ref_year, int ref_day_of_year, int ref_year_total, int calendar[]) {
 
     int day, month, year, year_total, days_since_ref, sched;
 
-    printf("Enter a future date:    ");
+    printf("Enter a future date (dd/mm/yy):    ");
     scanf("%d/%d/%d", &day, &month, &year);
     
     if (ref_year == year) {
